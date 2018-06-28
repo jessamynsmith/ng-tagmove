f1037fd650163507c1488009e07b19d84205ce02
K Gilmore <kgilmore.me@gmail.com>
2018-06-28 19:07:24 +0100

    fix menu icon bug2 with jquery

--- src/app/layouts/header/header.component.ts
+++ src/app/layouts/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import * as $ from 'jquery';
 
 @Component({
   selector: 'app-header',
@@ -10,6 +11,15 @@ export class HeaderComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+
+  //fix for menu close bug
+    $('.mobile-menu-tog').on('click', function(e) {
+      $('.mobile-menu-toggle').toggleClass("active");
+       $('#icon-menu-toggle').toggleClass("icon-x");
+       $('.mobile-menu').toggleClass("open");
+      e.preventDefault();
+    });
+
   }
 
 }
