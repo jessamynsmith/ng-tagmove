eed3fcd590428730b10c4f596b57595615b7047a
K Gilmore <kgilmore.me@gmail.com>
2018-06-30 13:44:14 +0100

    update product model

--- src/app/models/product.ts
+++ src/app/models/product.ts
@@ -1,18 +1,61 @@
 export interface Product {
     id: string,
+    categories: object[],
     title: string,
-    category: string,
+    price: string,
+    discountPrice: string,
     description: string,
+    offers: number,
+    vid: string,
+    coverImg: string,
+    imgs: object[],
+    option1: {
+      name: string,
+      price: string, 
+    },
+    option2: {
+      name: string,
+      price: string, 
+    },
+    fullDetails: string,
+    features: object[],
     spec: {
       dimensions: string,
       weight: string
     },
-    maxDiscount: string,
-    options: {
-      name: string,
-      price: string, 
-    },
-    sizes: object[],
-    colours: object[],
-    images: object[]
+    relatedProducts: object[]
   }
+
+  // {
+  //   "id": "",
+  //   "categories": [],
+  //   "title": "",
+  //   "price": 0,
+  //   "discountPrice": 0,
+  //   "description": "",
+  //   "offers": 0,
+  //   "vid": "",
+  //   "coverImg": "",
+  //   "imgs": [],
+  //   "option1": [
+  //     {
+  //       "name": "",
+  //       "price": ""
+  //     }
+  //   ],
+  //   "option2": [
+  //     {
+  //       "name": "",
+  //       "price": ""
+  //     }
+  //   ],
+  //   "fullDetails": "",
+  //   "features": [],
+  //   "spec": [
+  //     {
+  //       "name": "",
+  //       "value": ""
+  //     }
+  //   ],
+  //   "relatedProducts": []
+  // }
